807a1113e193330c65983cce8a163433b7823900
Chukwuma Nnodim-Amadi <C.NnodimAmadi001@umb.edu>
2022-06-23 00:40:34 -0400

    Add files via upload
    Added
    
    ~ More containers and visual borders
    ~ Text
    ~ Fixed container sizes and margins
    
    Issues
    
    ~ The responsiveness of the container messes with the website text when the screen-size is messed around with

--- home.js
+++ home.js
@@ -14,13 +14,35 @@ export default function Front() {
             <div className={styles.textBox}>
                 <h1 className={styles.kikiWhat}>WHAT IS KIKI?</h1>
             </div>
-
             <div className={styles.homeContainer}>
                 <div className={styles.homeContainer1}>
                     <div className={styles.imgContainer} />
                     <p className={styles.textContainer}>
-                </p>
-                    
+                    Kiki is a digital platform focused on sharing
+                    stories from black womxn with a special focus on authenticity
+                    and online community. Kiki was created with the intention of 
+                    being a safe-space for black womxn. It is statistically proven
+                    that a tenth of messages received online by black womxn 
+                    are abusive.
+                    Not only this, but they are not enough podcasts and digital
+                    platfroms that target primarily black womxn. This has ultimately
+                    led to the lacking of a safe online community for black womxn.
+                    Kiki was created to combat to combat this issue and more as the brand 
+                    flourishes.
+                    </p>
+                    <div className={styles.imgContainer} />
+                <div className={styles.homeContainer2} />
+
+                    <p className={styles.textContainer2}>
+
+                    </p>
+
+                    <div className={styles.imgContainer2} />
+
+                    <p className={styles.textContainer3}>
+
+                    </p>
+
                 </div>
             </div>
         </div>
